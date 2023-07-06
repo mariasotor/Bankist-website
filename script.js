f2272905cc050f9226549810930b684dda86c37c
mariasotor <alejandrasotor3@gmail.com>
2023-07-06 13:42:59 -0300

    Implemented smooth scrolling in navbar links

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 "use strict";
 
-///////////////////////////////////////
-// Modal window
+//Elements selected
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
+///////////////////////////////////////
+// Modal window
 const openModal = function (event) {
   event.preventDefault();
   modal.classList.remove("hidden");
@@ -30,10 +33,38 @@ document.addEventListener("keydown", function (event) {
   }
 });
 
-//Smooth scrolling
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
+//Page navigation
+/*document.querySelectorAll(".nav__link").forEach(function (el) {
+  el.addEventListener("click", function (event) {
+    event.preventDefault();
 
+    const id = this.getAttribute("href");
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});*/ //Not efficient
+
+//Using Event delegation
+//1. Add event listener to common parent element
+document
+  .querySelector(".nav__links")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    //2.Determine what element originated the event
+    const element = event.target;
+    if (
+      element.classList.contains("nav__link") &&
+      !element.classList.contains("nav__link--btn")
+    ) {
+      const id = element.getAttribute("href");
+      document.querySelector(id).scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+
+//Button smooth scrolling
 btnScrollTo.addEventListener("click", function (event) {
   section1.scrollIntoView({
     behavior: "smooth",
