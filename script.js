5e96deacf71d84d5b3e739614a4c2b2edaabc989
mariasotor <alejandrasotor3@gmail.com>
2023-07-07 17:33:07 -0300

    Implemented navbar behaviour
    Made navbar sticky after leaving the header

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 //Elements selected
+const header = document.querySelector(".header");
 const nav = document.querySelector(".nav");
 
 const btnScrollTo = document.querySelector(".btn--scroll-to");
@@ -122,3 +123,17 @@ const handlerHover = function (opacity) {
 nav.addEventListener("mouseover", handlerHover(0.5));
 
 nav.addEventListener("mouseout", handlerHover(1));
+
+//Sticky navigation: Intersection Observed API
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const obsOptions = { root: null, threshold: 0, rootMargin: `-${navHeight}px` };
+const observer = new IntersectionObserver(stickyNav, obsOptions);
+observer.observe(header);
