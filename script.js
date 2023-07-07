ba9098d6f8b9a94cf83efde74e8ac49b9758f109
mariasotor <alejandrasotor3@gmail.com>
2023-07-07 19:22:43 -0300

    Implemented lazy loading for images in section1

--- script.js
+++ script.js
@@ -142,7 +142,6 @@ headerObserver.observe(header);
 //Revealing sections animation
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) return;
   entry.target.classList.remove("section--hidden");
@@ -159,3 +158,28 @@ sections.forEach((section) => {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
 });
+
+//Lazy loading images
+const imgTargets = document.querySelectorAll(".features__img");
+
+const lazyLoad = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  //Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  //Remove blurry filter after img finish loading (and load event is emitted)
+  entry.target.addEventListener("load", () =>
+    entry.target.classList.remove("lazy-img")
+  );
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(lazyLoad, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
